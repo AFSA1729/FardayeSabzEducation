d973f10fdde6253a2b83c335c0c1860951b75bc9
Ashkan Majidi <ashkan.majidi.1382@gmail.com>
2023-05-27 15:17:06 +0330

    damn update_course_students_docs #1

--- Controller/StudentController.py
+++ Controller/StudentController.py
@@ -6,16 +6,22 @@ class StudentController:
     __student_documents_folder_id = "10iONxv6qOQCRYZoRGw7Cb6785jcYblVS"
 
     @staticmethod
+    def update_student_doc_folder(student_name: str, student_id: str, s: pd.Series):
+
+        pass
+    @staticmethod
+    def create_student_doc_folder(student_name: str, student_id: str, s: pd.Series):
+        title = student_id + '-' + student_name
+        folder = DriveController.create_folder(title, StudentController.__student_documents_folder_id)
+        gsheet = DriveController.create_gsheet(title, folder['id'])
+        df1, worksheet = DriveController.open_gsheet_as_df(gsheet['id'])
+        df = pd.DataFrame(columns=s.index.tolist())
+        df.loc[0] = s
+        worksheet.set_dataframe(df, (1, 1))
+
+    @staticmethod
     def student_id(student_id: int) -> str:
         string = student_id.__str__()
         while len(string) < 3:
             string = "0" + string
         return string
-
-    @staticmethod
-    def create_student_doc_folder(student_name: str, student_id: str, s: pd.Series):
-        title = student_name + '-' + student_id
-        folder = DriveController.create_folder(title, StudentController.__student_documents_folder_id)
-        gsheet = DriveController.create_gsheet(title, folder['id'])
-        df1, worksheet = DriveController.open_gsheet_as_df(gsheet['id'])
-        worksheet.set_dataframe(s.to_frame(), (1, 1))
